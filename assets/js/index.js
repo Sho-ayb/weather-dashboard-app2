b3d7575c974f2bf1509477fd8bc97270654d356e
Sho.ayb <Sho.ayb@outlook.com>
2023-02-21 17:39:38 +0000

    started index.js hid weather card and search link elements and attached event listener to search button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,24 @@
 console.log("index.js file loaded");
 
+// Global variables
+
+const apiKey = "d0af7ceac9a3501bc47a8577610395a2";
+
+// Query selecting page elements
+
+const cardContainerEl = document.getElementsByClassName("card__container");
+
+const searchLinksEl = document.querySelector(".search__links");
+
+const searchBtnEl = document.querySelector(".search__btn");
+
+const searchInputEl = document.getElementById("search__input");
+
+console.log(cardContainerEl);
+console.log(searchLinksEl);
+console.log(searchBtnEl);
+console.log(searchInputEl);
+
 //  use moment to get the current date
 const now = moment();
 // format the date here
@@ -10,8 +29,32 @@ const todaysDt = now.format("dddd, MMMM D");
 
 const dateEl = document.querySelector(".date");
 
-console.log(dateEl);
-
-// insert the formatted to the dateEl
+// insert the formatted date to dateEl element
 
 dateEl.textContent = todaysDt;
+
+// hiding the hard coded search links and weather cards from the page
+
+for (card of cardContainerEl) {
+  card.innerHTML = "";
+}
+
+searchLinksEl.innerHTML = "";
+
+// creating a function to listen to the form button
+
+const searchEvent = () => {
+  // adding an event listener to the forms search button
+
+  searchBtnEl.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    console.log("search button clicked");
+
+    const searchVal = searchInputEl.value.trim().toLowerCase();
+
+    console.log(searchVal);
+  });
+};
+
+searchEvent();
