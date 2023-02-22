12578e5677c9b6bbbe05236902e8b0f4c2591772
Sho.ayb <Sho.ayb@outlook.com>
2023-02-22 12:37:45 +0000

    event listener on search button working including error handling for a valid or invalid city entered

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,14 @@ console.log("index.js file loaded");
 
 const apiKey = "d0af7ceac9a3501bc47a8577610395a2";
 
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+
+// creating array to hold the users city searches
+
+const citySearches = window.localStorage.getItem("cities")
+  ? JSON.parse(window.localStorage.getItem("cities"))
+  : [];
+
 // Query selecting page elements
 
 const cardContainerEl = document.getElementsByClassName("card__container");
@@ -13,6 +21,8 @@ const searchLinksEl = document.querySelector(".search__links");
 const searchBtnEl = document.querySelector(".search__btn");
 
 const searchInputEl = document.getElementById("search__input");
+
+const formMessage = document.querySelector(".form__message");
 
 console.log(cardContainerEl);
 console.log(searchLinksEl);
@@ -33,7 +43,7 @@ const dateEl = document.querySelector(".date");
 
 dateEl.textContent = todaysDt;
 
-// hiding the hard coded search links and weather cards from the page
+// hiding the hard coded search links, weather cards and the form message from the page
 
 for (card of cardContainerEl) {
   card.innerHTML = "";
@@ -41,20 +51,60 @@ for (card of cardContainerEl) {
 
 searchLinksEl.innerHTML = "";
 
+formMessage.innerHTML = "";
+
 // creating a function to listen to the form button
 
-const searchEvent = () => {
+const searchEvent = async () => {
   // adding an event listener to the forms search button
 
-  searchBtnEl.addEventListener("click", function (e) {
+  searchBtnEl.addEventListener("click", async function (e) {
     e.preventDefault();
 
     console.log("search button clicked");
-
+    // getting the input from form input text field
     const searchVal = searchInputEl.value.trim().toLowerCase();
 
     console.log(searchVal);
+    // error handler for when no text entered in search field
+    if (!searchVal) {
+      formMessage.textContent = `Please enter a city name ! 😲`;
+    }
+
+    const status = await isCityValid(searchVal);
+
+    if (status === 404) {
+      formMessage.textContent = `The city does not exist, please try again! 😲`;
+    }
   });
+
+  // if form message exists clears the form message when user begins to type in to search field
+  searchInputEl.addEventListener("input", function () {
+    if (formMessage) {
+      formMessage.innerHTML = "";
+    }
+  });
+
+  // when there is some text entered and search button clicked
+  // when search button is clicked then check if the city entered is valid city
 };
+
+// function to check if the city is valid
+
+const isCityValid = async (city) => {
+  const queryUrl = `${apiUrl}${city}&appid=${apiKey}`;
+
+  const request = new Request(queryUrl, { method: "GET" });
+
+  const response = await fetch(request);
+
+  console.log(response);
+
+  return response.status;
+};
+
+// function to store city searches to local storage
+
+const storeSearchHistory = (city) => {};
 
 searchEvent();
